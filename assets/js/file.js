17381ab7a42cefaa527e9ca2e43a578c4662ccab
none <none@yhaoo.com>
2024-06-07 12:59:07 +0200

    create p in container clock

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,1 +1,7 @@
 "use strict";
+const divContenitoreClock = document.querySelector("#row-clock");
+const paragrafoDisplayClock = document.createElement("p");
+paragrafoDisplayClock.className = "display";
+paragrafoDisplayClock.id = "hours";
+paragrafoDisplayClock.innerHTML = '0';
+divContenitoreClock.appendChild(paragrafoDisplayClock);
