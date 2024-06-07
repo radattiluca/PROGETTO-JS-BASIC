2b15da6495ebf5b86a04565dd78d5ad21cdc6ddb
none <none@yhaoo.com>
2024-06-07 14:27:35 +0200

    create display countdown and button

--- assets/js/file.js
+++ assets/js/file.js
@@ -1,14 +1,46 @@
 "use strict";
+//creato contenitore clock con paragrafo all'interno del nodo row-clock
 const divContenitoreClock = document.querySelector("#row-clock");
 const paragrafoDisplayClock = document.createElement("p");
 paragrafoDisplayClock.className = "display";
-paragrafoDisplayClock.id = "hours";
+paragrafoDisplayClock.id = "display-clock";
 paragrafoDisplayClock.innerHTML = '0';
 divContenitoreClock.appendChild(paragrafoDisplayClock);
 
+//creato contenitore clock con paragrafo all'interno del nodo row-clock
 const divContenitoreCounter = document.querySelector("#row-counter");
 const paragrafoDisplayCounter = document.createElement("p");
 paragrafoDisplayCounter.className = "display";
 paragrafoDisplayCounter.id = "display-count";
 paragrafoDisplayCounter.innerHTML = '0';
 divContenitoreCounter.appendChild(paragrafoDisplayCounter);
+//creato pulsanti all'interno del nodo row-counter
+const pulsantePiu = document.createElement("button");
+const pulsanteReset = document.createElement("button");
+const pulsanteMeno = document.createElement("button");
+pulsantePiu.id = "plus";
+pulsantePiu.innerHTML = "+";
+pulsanteReset.id = "reset";
+pulsanteReset.innerHTML = "Reset";
+pulsanteMeno.id = "minus";
+pulsanteMeno.innerHTML = "-";
+divContenitoreCounter.append( pulsanteMeno, pulsanteReset, pulsantePiu);
+
+//creato contenitore countdown con paragrafo all'interno del nodo row-countdown
+const divContenitoreCountdown = document.querySelector("#row-countdown");
+const paragrafoDisplayCountdown = document.createElement("p");
+paragrafoDisplayCountdown.className = "display";
+paragrafoDisplayCountdown.id = "display-countdown";
+paragrafoDisplayCountdown.innerHTML = '0';
+divContenitoreCountdown.appendChild(paragrafoDisplayCountdown);
+//creato pulsanti all'interno del nodo countdown
+const pulsanteImposta = document.createElement("button");
+const pulsanteAvvia = document.createElement("button");
+const pulsanteStop = document.createElement("button");
+pulsanteImposta.id = "imposta";
+pulsanteImposta.innerHTML = "Imposta";
+pulsanteAvvia.id = "avvia";
+pulsanteAvvia.innerHTML = "Avvia";
+pulsanteStop.id = "stop";
+pulsanteStop.innerHTML = "Stop";
+divContenitoreCountdown.append( pulsanteAvvia, pulsanteImposta, pulsanteStop);
