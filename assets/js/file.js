2d015dbe49a756adfc82ffa00a283b6e01001b47
none <none@yhaoo.com>
2024-06-07 13:26:33 +0200

    create display counter

--- assets/js/file.js
+++ assets/js/file.js
@@ -5,3 +5,10 @@ paragrafoDisplayClock.className = "display";
 paragrafoDisplayClock.id = "hours";
 paragrafoDisplayClock.innerHTML = '0';
 divContenitoreClock.appendChild(paragrafoDisplayClock);
+
+const divContenitoreCounter = document.querySelector("#row-counter");
+const paragrafoDisplayCounter = document.createElement("p");
+paragrafoDisplayCounter.className = "display";
+paragrafoDisplayCounter.id = "display-count";
+paragrafoDisplayCounter.innerHTML = '0';
+divContenitoreCounter.appendChild(paragrafoDisplayCounter);
