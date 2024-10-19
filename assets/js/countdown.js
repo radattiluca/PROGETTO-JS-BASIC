a465420333c1f632234277ba79ab214f88439a6e
none <none@yhaoo.com>
2024-10-19 12:34:47 +0200

    changed the code set, seTwo and breakLong

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -34,56 +34,44 @@ set.addEventListener('click',function setTwentyFive(){
     
 
     start.addEventListener('click', function startCountDown(){
-        seTwo.disabled=true;//to not start the seTwo code if the user clicks on button 25 while the set code is running
-        
-        if(timeMinutes==24){
-            setTimeout(() => {
-                backTimeMinutes.textContent= timeMinutes; 
-            }, 999);
-        };
-
-
-        let timerMinutes = setInterval(function() {
-       
-        timeMinutes--;
-
-            if(timeMinutes <=9){
-                backTimeMinutes.textContent= `0${timeMinutes}`; 
-            }else{
-                backTimeMinutes.textContent= timeMinutes;
-            }
-
-            if(timeMinutes==0){
-                finishTimer();
-            }
-            
-        }, 60000);
+        seTwo.disabled=true; //to not start the seTwo code if the user clicks on button 5 while the set code is running
+        breakLong.disabled=true; //to not start the breakLong code if the user clicks on button 15 while the set code is running
 
         let timerSeconds = setInterval (function(){
+            
             timeSecond--;
-            backTimeSeconds.textContent= timeSecond;
-
+            
             if(timeSecond <=9){
                 backTimeSeconds.textContent= `0${timeSecond}`; 
             }else{
                 backTimeSeconds.textContent= timeSecond;
-            }
+            };
 
-            if(timeMinutes < 0){ 
-                backTimeMinutes.textContent= "00";
+            if (timeMinutes==0){
+                finishTimer(); 
+            };
+
+            if(timeMinutes==24){
+                    backTimeMinutes.textContent= `0${timeMinutes}`;
+            };
+
+            if(timeMinutes < 0){
                 clearInterval(timerSeconds);
-                clearInterval(timeMinutes);
-            }else if(timeSecond <=0){ 
+                backTimeMinutes.textContent= "00";
+                backTimeSeconds.textContent= "00";
+                
+            }else if(timeSecond <=0){
                 timeSecond = 60;
+                timeMinutes--;
+                backTimeMinutes.textContent= `0${timeMinutes}`;
                 timerSeconds();
-            }  
+            }; 
         }, 1000);
         
         stopCountDown.addEventListener('click', function(){
             //to freeze the pause button from running to less than a minute and avoid the execution of finishTimer()
             if(!(timeMinutes == 0)){
                 clearInterval(timerSeconds);
-                clearInterval(timerMinutes);
             }
         });    
     });
@@ -99,23 +87,7 @@ seTwo.addEventListener('click', function setFive(){
 
     start.addEventListener('click', function startCountDown(){
         set.disabled=true; //to not start the set code if the user clicks on button 25 while the seTwo code is running
-        
-        if(timeMinutes==4){
-            setTimeout(() => {
-                backTimeMinutes.textContent= `0${timeMinutes}` 
-            }, 999);
-        };
-
-        let timerMinutes = setInterval(function() {
-       
-            timeMinutes--;
-            backTimeMinutes.textContent= `0${timeMinutes}`;
-            
-            if(timeMinutes==0){
-                finishTimer();
-            }
-
-        }, 60000);
+        breakLong.disabled=true; //to not start the breakLong code if the user clicks on button 15 while the seTwo code is running.
 
         let timerSeconds = setInterval (function(){
             
@@ -125,23 +97,33 @@ seTwo.addEventListener('click', function setFive(){
                 backTimeSeconds.textContent= `0${timeSecond}`; 
             }else{
                 backTimeSeconds.textContent= timeSecond;
-            }
-            
+            };
+
+            if (timeMinutes==0){
+                finishTimer(); 
+            };
+
+            if(timeMinutes==4){
+                    backTimeMinutes.textContent= `0${timeMinutes}`;
+            };
+
             if(timeMinutes < 0){
-                backTimeMinutes.textContent= "00";
                 clearInterval(timerSeconds);
-                clearInterval(timerMinutes);
+                backTimeMinutes.textContent= "00";
+                backTimeSeconds.textContent= "00";
+                
             }else if(timeSecond <=0){
                 timeSecond = 60;
+                timeMinutes--;
+                backTimeMinutes.textContent= `0${timeMinutes}`;
                 timerSeconds();
-            }  
+            }; 
         }, 1000);
         
         stopCountDown.addEventListener('click', function(){
             //to freeze the pause button from running to less than a minute and avoid the execution of finishTimer()
             if(!(timeMinutes === 0)){ 
                 clearInterval(timerSeconds);
-                clearInterval(timerMinutes);
             }
         });    
     });
@@ -152,61 +134,50 @@ seTwo.addEventListener('click', function setFive(){
 //start of section break long 15 minutes
 breakLong.addEventListener('click', function setFifteen(){
     
+
     let timeMinutes = 14;
     let timeSecond = 60;
     backTimeMinutes.textContent = "15";
 
     start.addEventListener('click', function startCountDown(){
-        seTwo.disabled=true;//to not start the seTwo code if the user clicks on button 25 while the set code is running
+        set.disabled=true; //to not start the set code if the user clicks on button 25 while the seTwo code is running
+        seTwo.disabled=true; //to not start the seTwo code if the user clicks on button 5 while the seTwo code is running
         
-        if(timeMinutes==14){
-            setTimeout(() => {
-                backTimeMinutes.textContent= timeMinutes; 
-            }, 999);
-        };
-
-
-        let timerMinutes = setInterval(function() {
-       
-        timeMinutes--;
-
-            if(timeMinutes <=9){
-                backTimeMinutes.textContent= `0${timeMinutes}`; 
-            }else{
-                backTimeMinutes.textContent= timeMinutes;
-            }
-
-            if(timeMinutes==0){
-                finishTimer();
-            }
-            
-        }, 60000);
-
         let timerSeconds = setInterval (function(){
+            
             timeSecond--;
-            backTimeSeconds.textContent= timeSecond;
-
+            
             if(timeSecond <=9){
                 backTimeSeconds.textContent= `0${timeSecond}`; 
             }else{
                 backTimeSeconds.textContent= timeSecond;
-            }
+            };
 
-            if(timeMinutes < 0){ 
-                backTimeMinutes.textContent= "00";
+            if (timeMinutes==0){
+                finishTimer(); 
+            };
+
+            if(timeMinutes==14){
+                    backTimeMinutes.textContent= `0${timeMinutes}`;
+            };
+
+            if(timeMinutes < 0){
                 clearInterval(timerSeconds);
-                clearInterval(timeMinutes);
-            }else if(timeSecond <=0){ 
+                backTimeMinutes.textContent= "00";
+                backTimeSeconds.textContent= "00";
+                
+            }else if(timeSecond <=0){
                 timeSecond = 60;
+                timeMinutes--;
+                backTimeMinutes.textContent= `0${timeMinutes}`;
                 timerSeconds();
-            }  
+            }; 
         }, 1000);
         
         stopCountDown.addEventListener('click', function(){
             //to freeze the pause button from running to less than a minute and avoid the execution of finishTimer()
             if(!(timeMinutes === 0)){ 
                 clearInterval(timerSeconds);
-                clearInterval(timerMinutes);
             }
         });    
     });
